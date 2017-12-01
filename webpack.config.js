d61bfabca88170176cefdec75c0684813f5b7eee
Yu-Chia Wu <gerrywu@utexas.edu>
2017-12-01 15:31:38 -0600

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    app: './src/App.jsx',
+    vendor: ['react','react-dom','whatwg-fetch','babel-polyfill'],
+  },
+  output: {
+    path: './static',
+    filename: 'app.bundle.js'
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('vendor','vendor.bundle.js')
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react','es2015']
+        }
+      },
+    ]
+  },
+  devtool: 'source-map'
+};
