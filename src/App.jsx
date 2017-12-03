4720ec1ce8e1505d4e3722e9aef46295c6e02a75
Yu-Chia Wu <gerrywu@utexas.edu>
2017-12-03 10:54:23 -0600

    Wrap by container

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { Container } from 'semantic-ui-react'
 
 import VehicleList from './VehicleList.jsx';
 
 var contentNode = document.getElementById('contents');
-ReactDOM.render(<VehicleList />, contentNode);      // Render the component inside the content Node
+
+class App extends Component {
+  render() {
+    return (
+      <Container style={{ padding: '2em 0em' }}>
+        <VehicleList />
+      </Container>
+    )
+  };
+}
+
+ReactDOM.render(<App />, contentNode);      // Render the component inside the content Node
