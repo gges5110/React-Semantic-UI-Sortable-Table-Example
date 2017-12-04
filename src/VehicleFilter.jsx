fc68f9c2ed59310aa8d9c61dfbe91d01fb63ea35
Yu-Chia Wu <gerrywu@utexas.edu>
2017-12-03 22:07:00 -0600

    clean up

--- src/VehicleFilter.jsx
+++ src/VehicleFilter.jsx
@@ -8,28 +8,28 @@ export default class VehicleFilter extends React.Component {
       filter: ''
     }
     this.handleOnChange = this.handleOnChange.bind(this);
-    this.handleOnSubmit = this.handleOnSubmit.bind(this);
   }
 
   handleOnChange(event, {name, value}) {
-    console.log('handle on change');
     this.setState({[name]: value});
     this.props.onSubmitFilter(value);
-  }
-
-  handleOnSubmit() {
-    console.log('submit: ' + this.state.filter);
-    this.props.onSubmitFilter(this.state.filter);
   }
 
   render() {
     const { filter } = this.state;
     return (
-      <Form onSubmit={this.handleOnSubmit}>
+      <Form>
         <Form.Group>
           <Form.Field>
-            <label>Filter</label>
-            <Form.Input placeholder='Enter the filter.' name='filter' value={filter} onChange={this.handleOnChange}/>
+            <Form.Input
+              placeholder='Enter the filter.'
+              name='filter'
+              value={filter}
+              error={this.props.totalCount === 0}
+              label='Filter'
+              onChange={this.handleOnChange}
+              icon='search'
+            />
           </Form.Field>
         </Form.Group>
       </Form>
