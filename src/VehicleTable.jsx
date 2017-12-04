cd76d49ef1b5606a76f34cdfdd750400446da028
Yu-Chia Wu <gerrywu@utexas.edu>
2017-12-03 22:07:56 -0600

    Update
    Set column width
    Vehicle table
    Vehicle row

--- src/VehicleTable.jsx
+++ src/VehicleTable.jsx
@@ -1,9 +1,82 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Button, Table, Checkbox, Icon } from 'semantic-ui-react'
 
-export default class VehicleTable extends React.Component {
-  render() {
-    return (
-      <div>This is a placeholder for the VehicleTable.</div>
-    )
+import VehicleTablePagination from './VehicleTablePagination.jsx';
+
+class VehicleRow extends React.Component {
+  constructor() {
+    super();
+    this.addToFavorite = this.addToFavorite.bind(this);
   }
+
+  addToFavorite() {
+    this.props.addFavorite(this.props.vehicle);
+  }
+
+  render() {
+    const icon = this.props.vehicle.favorite ? <Icon name='heart'/> : <Icon name='empty heart' />;
+    return (
+        <Table.Row>
+          <Table.Cell>{this.props.vehicle._id}</Table.Cell>
+          <Table.Cell>{this.props.vehicle.make}</Table.Cell>
+          <Table.Cell>{this.props.vehicle.model}</Table.Cell>
+          <Table.Cell>{this.props.vehicle.year}</Table.Cell>
+          <Table.Cell>{this.props.vehicle.package}</Table.Cell>
+          <Table.Cell>{this.props.vehicle.fuelType}</Table.Cell>
+          <Table.Cell>{this.props.vehicle.transmission}</Table.Cell>
+          <Table.Cell textAlign='center'>
+            <Button
+              onClick={this.addToFavorite}
+              color={this.props.vehicle.favorite? 'google plus' : 'teal'}
+              icon={this.props.vehicle.favorite ? 'heart' : 'heart outline'} />
+          </Table.Cell>
+        </Table.Row>
+
+    );
+  }
+}
+
+VehicleRow.propTypes = {
+  vehicle: PropTypes.object.isRequired,
+}
+
+export default function VehicleTable(props) {
+  const vehicleRows = props.vehicles.map(vehicle => <VehicleRow key={vehicle._id} vehicle={vehicle} addFavorite={props.addFavorite} />)
+  return (
+    <div>
+      Total count: {props.totalCount}
+      <Table celled selectable>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell width={1}>#</Table.HeaderCell>
+            <Table.HeaderCell width={3}>Make</Table.HeaderCell>
+            <Table.HeaderCell width={3}>Model</Table.HeaderCell>
+            <Table.HeaderCell width={1}>Year</Table.HeaderCell>
+            <Table.HeaderCell width={1}>Package</Table.HeaderCell>
+            <Table.HeaderCell width={1}>Fuel Type</Table.HeaderCell>
+            <Table.HeaderCell width={1}>Transmission</Table.HeaderCell>
+            <Table.HeaderCell width={1} >Favorite</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          {vehicleRows}
+        </Table.Body>
+
+        <Table.Footer>
+          <VehicleTablePagination
+            totalPages={props.totalPages}
+            currentPage={props.currentPage}
+            onChangePage={props.onChangePage}
+          />
+        </Table.Footer>
+      </Table>
+    </div>
+  );
+}
+
+VehicleTable.propTypes = {
+  vehicles: PropTypes.array.isRequired,
+  onChangePage: PropTypes.func.isRequired,
 }
