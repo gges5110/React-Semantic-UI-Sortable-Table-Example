bc6a42e78d48c05f019dfa032677467b9eaa0dae
gw01720 <gw01720@rmn.com>
2018-08-12 23:41:56 -0500

    Fix test

--- src/VehicleFilter.spec.js
+++ src/VehicleFilter.spec.js
@@ -3,7 +3,17 @@ import { shallow } from 'enzyme';
 import VehicleFilter from './VehicleFilter';
 describe('VehicleFilter', () => {
   it('should render correctly in "debug" mode', () => {
-    const component = shallow(<VehicleFilter debug />);
+    let filter = '';
+    let totalCount = 100;
+    let onSubmitFilter = function() {
+
+    }
+
+    const component = shallow(<VehicleFilter
+      filter = { filter }
+      totalCount = { totalCount }
+      onSubmitFilter = { onSubmitFilter }
+      debug />);
 
     expect(component).toMatchSnapshot();
   });
