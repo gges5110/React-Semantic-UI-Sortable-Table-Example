de3faa71a4e6acc0754b1c0115d082cd92f5af19
gw01720 <gw01720@rmn.com>
2018-08-17 21:56:49 -0500

    Integrate json-server with express server to serve React at the same time

--- server/server.js
+++ server/server.js
@@ -1,17 +1,24 @@
+const express = require('express');
 const jsonServer = require('json-server');
-const server = jsonServer.create();
+// const server = jsonServer.create();
+const server = express();
 const path = require('path')
 const router = jsonServer.router(path.join(__dirname, 'vehicles.json'))
-const middlewares = jsonServer.defaults();
+// const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
-server.use(middlewares);
+server.use('/api/v1', router);
+
+// server.use(middlewares);
+server.use(express.static(path.join(__dirname, '/../static/')));
+
+server.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/../static/index.html'));
+});
 
 // Add this before server.use(router)
-server.use(jsonServer.rewriter({
-  '/api/v1/*': '/$1'
-}))
-
-server.use(router);
+// server.use(jsonServer.rewriter({
+//   '/api/v1/*': '/$1'
+// }))
 
 server.listen(port);
--- src/VehicleTable.jsx
+++ src/VehicleTable.jsx
@@ -44,6 +44,9 @@ VehicleRow.propTypes = {
 }
 
 export default function VehicleTable(props) {
+  if (props.vehicles === undefined) {
+    return <div></div>;
+  }
   const vehicleRows = props.vehicles.map(vehicle => <VehicleRow key={vehicle._id} vehicle={vehicle} addFavorite={props.addFavorite} />)
   return (
     <div>
